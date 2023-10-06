6348f60b35a9ab9bdca357158d66f621a0abdc8e
SoltaneKarim <karimsoltane92@gmail.com>
2023-10-06 15:40:06 +0100

    profile update

--- front-end/app/index.js
+++ front-end/app/index.js
@@ -1,14 +1,13 @@
 import React , { useState , useEffect } from "react";
-import { View , Text , StyleSheet , SafeAreaView , Image ,ScrollView} from "react-native";
+import { View , Text , StyleSheet , SafeAreaView , Image ,ScrollView, } from "react-native";
 import { Stack,useRouter } from "expo-router";
 import Login from "../components/Login/Login.jsx";
-
 import Loader from "../components/Loader/Loader.jsx";
-
 import Chou from "../components/chatbest/chatbest.jsx";
 import Identify from "../components/identify/identify.jsx";
 import Test from "../components/test/test.jsx";
 import Groups from "../components/groups/groups.jsx";
+import  Profile  from "../components/Profile/Profile.jsx";
 const App = () => {
 
   
@@ -16,7 +15,7 @@ const App = () => {
     return (
       <SafeAreaView style={styles.container}>
     
-         <Loader />
+         <Profile />
 
       </SafeAreaView>
     );
--- front-end/components/Profile/Profile.jsx
+++ front-end/components/Profile/Profile.jsx
@@ -1,255 +1,388 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Text, Image, TouchableOpacity, TextInput } from "react-native";
-import { Color, } from "./GlobalStyles";
-import { UseSelector, useSelector } from "react-redux"
-const Profile = () => {
-  const currentUser = useSelector((state) => state.user); // Assuming your user object in Redux has a 'name' property
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity, TextInput, Alert, StyleSheet, Modal } from 'react-native';
 
-  const [walletAmount, setWalletAmount] = useState(0);
-  
-  const handleLogoutClick = () => {
-    console.log("Logout clicked");
+const ProfileComponent = () => {
+  const [wallet, setWallet] = useState(0);
+  const [amount, setAmount] = useState(0);
+  const [password, setPassword] = useState('');
+  const [isPasswordDialogVisible, setIsPasswordDialogVisible] = useState(false);
+  const [isWalletAlertVisible, setIsWalletAlertVisible] = useState(false);
+  const [isWalletInputVisible, setIsWalletInputVisible] = useState(false);
+  const [isIncrementing, setIsIncrementing] = useState(true); // State to track if user wants to increment (true) or decrement (false)
+
+  const handleIncrement = () => {
+    setIsPasswordDialogVisible(true);
+    setIsIncrementing(true); // User wants to increment
   };
 
-  const handlePlusClick = () => {
-    const incrementedAmount = walletAmount + 1;
-    setWalletAmount(incrementedAmount);
+  const handleDecrement = () => {
+    setIsPasswordDialogVisible(true);
+    setIsIncrementing(false); // User wants to decrement
   };
 
-  const handleMinusClick = () => {
-    const decrementedAmount = walletAmount - 1;
-    setWalletAmount(decrementedAmount);
+  const handleConfirmPassword = () => {
+    if (password === 'yourpassword') { // Replace 'yourpassword' with the actual password
+      if (isPasswordDialogVisible) {
+        if (amount !== 0) {
+          if (isIncrementing) {
+            setWallet(wallet + amount);
+          } else {
+            setWallet(wallet - amount);
+          }
+          setIsPasswordDialogVisible(false);
+          setAmount(0);
+          setPassword('');
+        } else {
+          Alert.alert('Invalid Amount', 'Please enter a valid amount.');
+        }
+      }
+    } else {
+      setIsPasswordDialogVisible(false);
+      setPassword('');
+      Alert.alert('Invalid Password', 'Please enter the correct password.');
+    }
+  };
+
+  const handleCancelPassword = () => {
+    setIsPasswordDialogVisible(false);
+    setPassword('');
+  };
+
+  // Function to handle the Wallet text click
+  const handleWalletTextClick = () => {
+    setIsWalletInputVisible(true);
+  };
+
+  const handleWalletInputCancel = () => {
+    setIsWalletInputVisible(false);
+  };
+
+  const handleWalletInputConfirm = () => {
+    setIsWalletInputVisible(false);
+    // You can add logic here to handle the input value
   };
 
   return (
-    <View style={styles.profile}>
+    <View style={styles.container}>
       <Image
-        style={[styles.headerIcon, styles.iconLayout]}
-        contentFit="cover"
-        source={require("../../assets/profile/header.png")}
+        source={require('../../assets/profile/DEFAULT.png')} // Replace with your image source
+        style={styles.profileImage}
       />
+      <Text style={styles.name}>John Doe</Text>
       <Image
-        style={[styles.oval2Icon, styles.iconLayout]}
-        contentFit="cover"
-        source={require("../../assets/profile/Oval-1.png")}
+        source={require('../../assets/profile/verified-logo.png')} // Replace with your verified logo source
+        style={styles.verifiedLogo}
       />
-      <View style={styles.line} />
-      <Text style={[styles.sanFranciscoUsa, styles.jessicaJones27Clr]}>
-        San Francisco, USA
-      </Text>
-      <Text style={[styles.jessicaJones27, styles.jessicaJones27Clr]}>
-        Jessica Jones, 27
-      </Text>
-
-      <TouchableOpacity  onPress={() => handleLogoutClick("LOGOUT")}>
-        <View style={styles.message}>
-          <View style={styles.bgPosition} />
-          <Text style={styles.message1Typo}>LOGOUT</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity>
-      <Image style={styles.imageDefault}
-        contentFit="cover"
-        source={{uri:currentUser?.imageUrl}}
-      />
-      </TouchableOpacity>
-      <View style={styles.money}>
-        <TouchableOpacity onPress={handlePlusClick}>
-          <Text style={[styles.text, styles.textTypo]}>+</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handleMinusClick}>
-          <Text style={[styles.text1, styles.textTypo]}>-</Text>
-        </TouchableOpacity>
-        <TextInput
-          style={[styles.text2, styles.text2Typo]}
-          value={walletAmount.toString()}
-          keyboardType="numeric"
-          onChangeText={(newAmount) => setWalletAmount(parseFloat(newAmount))}
-        />
-
-        <Text style={[styles.spinach, styles.text2Typo]}>WALLET</Text>
-        <Image
-          style={[styles.wallet11, styles.iconLayout]}
-          contentFit="cover"
-          source={require("../../assets/profile/wallet-1-1.png")}
-        />
+      <Text style={styles.address}>123 Main St, City</Text>
+      <View style={styles.logoGrid}>
+        {/* Add your logos here */}
+        <Image source={require('../../assets/animal6.png')} style={styles.logo} />
+        <Image source={require('../../assets/shep4.png')} style={styles.logo} />
+        <Image source={require('../../assets/tree.png')} style={styles.logo} />
+        {/* Add more logos as needed */}
       </View>
+      <View style={styles.walletContainer}>
+        <TouchableOpacity onPress={handleWalletTextClick} style={styles.walletInputContainer}>
+          <Text style={styles.walletText}>Wallet: {wallet} TND</Text>
+        </TouchableOpacity>
+      </View>
+      {isPasswordDialogVisible && (
+        <View style={styles.confirmationContainer}>
+          <TextInput
+            style={styles.passwordInput}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry
+            placeholder="Enter Password"
+          />
+          <TouchableOpacity onPress={handleCancelPassword} style={styles.cancelButton}>
+            <Text style={styles.cancelButtonText}>Cancel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleConfirmPassword} style={styles.confirmButton}>
+            <Text style={styles.confirmButtonText}>Confirm</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
+      {/* Wallet input modal */}
+      <Modal
+        visible={isWalletInputVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={handleWalletInputCancel}
+      >
+        <View style={styles.walletInputModal}>
+          <Text style={styles.walletInputLabel}>Enter Amount:</Text>
+          <View style={styles.amountContainer}>
+            <TouchableOpacity onPress={() => setAmount(amount + 1)} style={styles.amountButton}>
+              <Text style={styles.amountButtonText}>+</Text>
+            </TouchableOpacity>
+            <Text style={styles.amountText}>{amount}</Text>
+            <TouchableOpacity onPress={() => setAmount(amount - 1)} style={styles.amountButton}>
+              <Text style={styles.amountButtonText}>-</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity onPress={handleWalletInputCancel} style={styles.walletInputCancelButton}>
+            <Text style={styles.walletInputCancelButtonText}>Cancel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleWalletInputConfirm} style={styles.walletInputConfirmButton}>
+            <Text style={styles.walletInputConfirmButtonText}>Confirm</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  iconLayout: {
-    maxHeight: "100%",
-    overflow: "hidden",
-    maxWidth: "100%",
-    position: "absolute",
+  container: {
+    alignItems: 'center',
+    padding: 20,
   },
-  jessicaJones27Clr: {
-    color: Color.colorDarkslateblue,
-    textAlign: "left",
-    position: "absolute",
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50, // to make it round
   },
- 
-  bgPosition: {
-    shadowOpacity: 1,
-    shadowOffset: {
-      width: 0,
-      height: 2.7961666584014893,
-    },
-    backgroundColor: Color.colorDarkslategray_100,
-    bottom: "0%",
-    height: "2000%",
-    left: "-71%",
-    top: "11500%",
-    position: "absolute",
-    width: "120%",
-    borderRadius:5
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
   },
-  message1Typo: {
-    color: Color.colorWhite,
-    fontWeight: "700",
-    letterSpacing: 0,
-    fontSize: 14,
-    top: "12100%",
-    textAlign: "center",
-    lineHeight: 15,
-    left:-73,
-    height:20
+  verifiedLogo: {
+    width: 20,
+    height: 20,
+    marginLeft: 5,
   },
-  hoverPosition: {
-    display: "none",
-    position: "absolute",
+  address: {
+    marginTop: 5,
   },
-  bg1ShadowBox: {
-    elevation: 7.61,
-    shadowRadius: 7.61,
-    shadowColor: "rgba(50, 50, 93, 0.1)",
-    borderRadius: 2,
+  logoGrid: {
+    flexDirection: 'row',
+    marginTop: 20,
   },
-  
-  text2Typo: {
-    fontWeight: "700",
-    letterSpacing: 1,
-    position: "absolute",
+  logo: {
+    width: 50,
+    height: 50,
+    margin: 5,
   },
-  headerIcon: {
-    height: "42.8%",
-    width: "100.1%",
-    right: "-0.1%",
-    bottom: "57.2%",
-    left: "0%",
-    top: "0%",
-    overflow: "hidden",
-    maxWidth: "100%",
+  walletContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
   },
-  oval2Icon: {
-    height: "12.76%",
-    width: "24.14%",
-    top: "14.95%",
-    right: "62.77%",
-    bottom: "72.29%",
-    left: "13.09%",
-    opacity: 0.1,
+  amountInput: {
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    width: 100,
+    textAlign: 'center',
+    fontSize: 18,
   },
-  line: {
-    height: "0.2%",
-    width: "56.54%",
-    top: "38.26%",
-    right: "21.52%",
-    bottom: "61.64%",
-    left: "21.94%",
-    backgroundColor: "#e9ecef",
-    position: "absolute",
+  walletButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    marginHorizontal: 5,
   },
-  sanFranciscoUsa: {
-    top: "28%",
-    left: "35%",
-    fontSize: 16,
-    fontWeight: "400",
-    textAlign: "left",
-    lineHeight: 15,
-    color: Color.colorDarkslateblue,
+  walletButtonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
-  jessicaJones27: {
-    top: "23%",
-    fontSize: 23,
-    lineHeight: 22,
-    left: "29.5%",
-    textAlign: "left",
+  walletText: {
+    marginTop: 20,
+    fontSize: 18,
+    fontWeight: 'bold',
   },
-  message: {
-    height: "5%",
-    width: "30%",
-    top: "52.5%",
-    right: "30.16%",
-    left: "55%",
+  confirmationContainer: {
+    marginTop: 20,
+    alignItems: 'center',
   },
-  text: {
-    width: "14%",
-    left: "90%",
-    color: "#4a9c4a",
-    fontSize:60,
-    top:210
+  passwordInput: {
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    width: 200,
+    textAlign: 'center',
+    fontSize: 18,
   },
-  text1: {
-    width: "12%",
-    color: "#da1717",
-    left: "0%",
-    fontSize:100,
-    top:100
-    
+  confirmButton: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
   },
-  text2: {
-    height: "20%",
-    width: "92.63%",
-    top: "51.42%",
-    left: "4.56%",
-    fontSize: 33,
-    color: "#1b5630",
-    textAlign: "center",
-    fontWeight: "500",
-    letterSpacing: 1,
+  confirmButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
-  spinach: {
-    height: "10%",
-    width: "42%",
-    top: "74%",
-    left: "33%",
-    fontSize: 23,
-    color: "#282c29",
+  cancelButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
   },
-  wallet11: {
-    height: "47%",
-    width: "62.92%",
-    right: "18.7%",
-    bottom: "56.34%",
-    left: "18.38%",
-    top: "0%",
-    overflow: "hidden",
-    maxWidth: "100%",
+  cancelButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
-  money: {
-    height: "41.95%",
-    width: "54.81%",
-    top: "47.1%",
-    right: "24.68%",
-    bottom: "10.94%",
-    left: "20.51%",
-    position: "absolute",
+  walletInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'relative',
   },
-  profile: {
+  walletText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+
+
+  walletInputModal: {
     flex: 1,
-    height: 535,
-    width: "100%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
-  imageDefault: {
-    width: 150,
-    height: 150, // Increase the height to match the width to make it a circle
-    top: -20,
-    left: 130,
-    borderRadius: 90, // Half of the width and height
-  }
+  walletInput: {
+    width: 200,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  walletInputCancelButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+  },
+  walletInputCancelButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  walletInputConfirmButton: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+  },
+  walletInputConfirmButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  walletInputModal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  walletInputLabel: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  amountContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  amountButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  amountButtonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  amountText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginHorizontal: 20,
+  },
+  walletInputCancelButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+  },
+  walletInputCancelButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  walletInputConfirmButton: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+  },
+  walletInputConfirmButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  walletContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  walletButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    marginHorizontal: 5,
+  },
+  walletButtonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+  // Modal styles
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalInput: {
+    width: 200,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    backgroundColor: 'white',
+  },
+  modalCancelButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+  },
+  modalCancelButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  modalConfirmButton: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+  },
+  modalConfirmButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 });
 
-export default Profile;
+
+export default ProfileComponent;
